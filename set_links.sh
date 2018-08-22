76cc8a126827fd8ca6f64dba1afcbce9a158e210
CarlosEDP <me@carlosedp.com>
2018-08-22 17:20:18 +0100

    Missing .profile

--- set_links.sh
+++ set_links.sh
@@ -1,7 +1,7 @@
 echo "Setting dotfiles on ~/"
 echo `pwd`
 
-for FILE in aliases bashrc erlang gitconfig gitignore inputrc screenrc ssh vimrc conf
+for FILE in aliases bashrc erlang gitconfig gitignore inputrc screenrc ssh vimrc conf profile
 do
     ln -sf `pwd`/$FILE ~/.$FILE
 done
