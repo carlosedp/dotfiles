910266179fe8d6a42bc7b84b0d94ccdb2fd85f76
CarlosEDP <me@carlosedp.com>
2018-10-23 18:17:09 -0300

    Fix links script. Do not autoindent VIM"

--- set_links.sh
+++ set_links.sh
@@ -3,6 +3,9 @@ echo `pwd`
 
 for FILE in `pwd`/rc/*
 do
+    if [[ -f ~/.$(basename $FILE) || -d ~/.$(basename $FILE) ]] && [ ! -L ~/.$(basename $FILE) ]; then
+      mv ~/.$(basename $FILE) ~/.$(basename $FILE)-old
+    fi
     ln -sf $FILE ~/.$(basename $FILE)
 done
 
