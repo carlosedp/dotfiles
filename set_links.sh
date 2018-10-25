0bd956e98b43fe951c4a09df16b08c3a54c43bc5
CarlosEDP <me@carlosedp.com>
2018-10-24 21:52:10 -0300

    Update set_links function to link additional configs

--- set_links.sh
+++ set_links.sh
@@ -1,12 +1,45 @@
+#!/bin/bash
+
 echo "Setting dotfiles on ~/"
 echo `pwd`
 
+# Settings
+sync_folder="$HOME/Google\ Drive"
+container_settings='com.termius.mac'
+application_support_settings='iTerm2 Franz'
+
+
+create_link() {
+  origin=$1
+  dest=$2
+  echo Linking origin file \"$origin\" to destination \"$dest\"
+
+  if [[ -f "$dest" || -d "$dest" ]] && [ ! -L "$dest" ]; then
+      echo "Destination already exists. Renaming to $dest-old"
+      mv "$dest" "$dest-old"
+  fi
+  ln -sf "$origin" "$dest"
+}
+
+
+# Link .rc files
 for FILE in `pwd`/rc/*
 do
-    if [[ -f ~/.$(basename $FILE) || -d ~/.$(basename $FILE) ]] && [ ! -L ~/.$(basename $FILE) ]; then
-      mv ~/.$(basename $FILE) ~/.$(basename $FILE)-old
-    fi
-    ln -sf $FILE ~/.$(basename $FILE)
+  create_link $FILE ~/.$(basename $FILE)
 done
 
-ln -sf ~/Google\ Drive/SSH_Keys ~/.ssh
+# Link SSH keys
+ln -sf "$sync_folder/SSH_Keys" ~/.ssh
+
+# Link settings from ~/Library/Containers
+for X in $container_settings
+do
+  create_link "$sync_folder/Configs/$X" "$HOME/Library/Containers/$X"
+done
+
+# Link settings from ~/Library/Application Support
+for X in $application_support_settings
+do
+  create_link "$sync_folder/Configs/$X" "$HOME/Library/Application Support/$X"
+done
+
