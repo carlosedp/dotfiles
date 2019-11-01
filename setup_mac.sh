6656d195fc79ff59ec2f03a6705ad61b709e0414
Carlos de Paula <me@carlosedp.com>
2019-11-01 10:19:24 -0300

    Use Brewfile instead of script to install mac applications

--- setup_mac.sh
+++ setup_mac.sh
@@ -32,36 +32,12 @@ else
 fi
 sleep 3
 
-echo "Install the rest of important brews"
+echo "Install brews"
 echo "==================================="
 echo ""
-# Install basic apps (git, wget, etc)
-HOMEBREW_NO_AUTO_UPDATE=1
-brew install \
-    git \
-    hub \
-    wget \
-    ack \
-    markdown \
-    sshuttle \
-    htop \
-    jq \
-    bat \
-    autojump \
-    tree \
-    youtube-dl \
-    gawk \
-    rlwrap \
-    xz \
-    neofetch \
-    gnu-tar
-
-# Cleanup old installs
-brew update
-brew upgrade
-brew cleanup
+brew bundle install
 echo ""
-echo "done ... Installing extra brews"
+echo "done ..."
 echo ""
 sleep 1
 
@@ -87,66 +63,6 @@ bash -c $DOTFILES/setup_tmux.sh
 
 # Setup OsX defaults
 bash -c $DOTFILES/osx_prefs.sh
-
-# Brew and additional commands
-brew tap buo/cask-upgrade
-brew tap beeftornado/rmtree
-
-# Macbook pro key repeats fix
-brew cask install unshaky
-
-# My taps
-brew tap carlosedp/tap
-brew install sshoot
-
-# Docker
-brew cask install docker
-
-# Docker and Kubernetes packages
-brew install kubernetes-cli \
-             docker-completion \
-             kubectx
-
-# Footloose (https://github.com/weaveworks/footloose/)
-brew tap weaveworks/tap
-brew install weaveworks/tap/footloose
-
-# Kubernetes Logging
-brew install stern
-brew install peco
-
-# Dive, container image inspection tool
-brew tap wagoodman/dive
-brew install dive
-
-# Editor and Terminal
-echo "Installing iTerm2, VSCode and fonts/utilities"
-echo ""
-brew cask install iterm2
-brew cask install visual-studio-code
-brew tap homebrew/cask-fonts
-brew cask install font-firacode-nerd-font
-brew cask instal kdiff3
-brew cask install open-in-code
-
-# Notes
-brew cask install joplin
-
-# Ansible and sshpass
-brew install ansible
-brew install hudochenkov/sshpass/sshpass
-
-# Markdown extensions
-brew cask install qlmarkdown
-brew cask install qlcolorcode
-
-# Quicklook extensions
-brew cask install quicklook-json
-brew cask install qlstephen
-
-# Install Programming languages
-brew install python
-brew install go
 
 # Add TouchID authentication to Sudo
 if [[ ! `grep "pam_tid.so" /etc/pam.d/sudo` ]]; then
