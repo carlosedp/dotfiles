6602949300f5c49c9b9acde26eb80c0d9e1802e6
Carlos de Paula <me@carlosedp.com>
2019-12-27 14:48:32 -0300

    Fix path

--- shellconfig/path.sh
+++ shellconfig/path.sh
@@ -1,4 +1,4 @@
-export PATH="~/.dotfiles/bin:$PATH"
+export PATH="$HOME/.dotfiles/bin:$PATH"
 export PATH="/usr/local/sbin:$PATH"
 export PATH="/work/riscv-go/misc/riscv:/work/riscv-go/bin:$PATH"
-export PATH=:"/usr/local/opt/coreutils/libexec/gnubin:$PATH"
+export PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH"
