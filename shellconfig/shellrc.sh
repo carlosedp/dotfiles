29afc05380afc78ea1c77b7b6eb75c48e63596af
Carlos de Paula <me@carlosedp.com>
2020-01-31 23:40:12 +0000

    Show if there are tmux sessions active

--- shellconfig/shellrc.sh
+++ shellconfig/shellrc.sh
@@ -53,3 +53,11 @@ fi
 if [ -x "$(command -v neofetch)" ] > /dev/null 2>&1; then
     neofetch --disable packages
 fi
+
+if [ "$(tmux list-sessions)" ] > /dev/null 2>&1; then
+    echo ""
+    echo "There are TMux sessions running:"
+    echo ""
+    tmux list-sessions
+    echo ""
+fi
