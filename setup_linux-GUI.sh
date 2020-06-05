5df3df41dca506e6a2150d4d8f0f0f1db608a974
Carlos de Paula <me@carlosedp.com>
2020-06-05 17:44:26 -0300

    Add Linux GUI setup script

---
+++ setup_linux-GUI.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+DOTFILES=$HOME/.dotfiles
+cd $HOME
+
+BASEPACKAGES="terminator"
+
+# Install Linux packages
+source /etc/os-release
+if [ $ID == "debian" ] || [ $ID == "ubuntu" ]; then
+    sudo apt update
+    sudo apt upgrade -y
+    for i in $BASEPACKAGES; do
+        sudo apt install --no-install-recommends $i
+    done
+elif [ $ID == "fedora" ] || [ $ID == "centos" ]; then
+    sudo dnf update -y
+    sudo dnf install -y $BASEPACKAGES
+elif [ $ID == "alpine" ]; then
+    sudo apk update
+    sudo apk add $BASEPACKAGES
+elif [ $ID == "void" ]; then
+    sudo xbps-install -Su -y $BASEPACKAGES
+fi
+
+# Install Fonts
+sudo tar vxf $HOME/.dotfiles/fonts/FC.tar.gz -C /usr/share/fonts
+sudo tar vxf $HOME/.dotfiles/fonts/NF.tar.gz -C /usr/share/fonts
+
+# Install VS Code
+if [ $ID == "debian" ] || [ $ID == "ubuntu" ]; then
+    sudo bash -c ". <( wget -O - https://code.headmelted.com/installers/apt.sh )"
+elif [ $ID == "fedora" ] || [ $ID == "centos" ]; then
+    sudo bash -c ". <( wget -O - https://code.headmelted.com/installers/yum.sh )"
+fi
+sudo ln -sf /usr/bin/code-oss /usr/local/bin/code
+
+
+echo "Setup finished!"
