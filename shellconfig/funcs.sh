426b40db1679d2a5d45d5916553abfb3a0fb7a5b
Carlos de Paula <me@carlosedp.com>
2020-06-17 15:08:12 -0300

    Add SSH function thru fzf

--- shellconfig/funcs.sh
+++ shellconfig/funcs.sh
@@ -2,7 +2,7 @@
 
 # Generate a scp command to copy files between hosts
 function scppath () {
-    echo $USER@`hostname -I | awk '{print $1}'`:`readlink -f $1`;
+    echo $USER@$(hostname -I | awk '{print $1}'`:`readlink -f $1);
 }
 
 # Call journalctl for process or all if no arguments
@@ -54,4 +54,14 @@ function rg() {
 # Install latest Golang. Replaces current one on /usr/local/go
 function install_golang() {
     if $(ldd --version 2>&1 | grep -i musl > /dev/null); then echo "ERROR: Distro not supported for Go.";return 1; fi && declare -A ARCH=( [x86_64]=amd64 [aarch64]=arm64 [armv7l]=arm [ppc64le]=ppc64le [s390x]=s390x ); FILE=$(curl -sL https://golang.org/dl/?mode=json | grep -E 'go[0-9\.]+' | sed 's/.*\(go.*\.tar\.gz\).*/\1/' | sort -n | grep -i $(uname -s) | grep tar | grep ${ARCH[$(uname -m)]} | tail -1); curl -sL https://dl.google.com/go/$FILE -o $FILE && sudo rm -rf /usr/local/go && sudo tar vxf $FILE -C /usr/local/ 2>/dev/null && echo "Installed $FILE" || echo "Error installing Go"
+}
+
+# Searches SSH hosts thru fzf and connects to it
+function ss() {
+    filter=${1:-"."}
+    target=$(egrep -o "Host (\b.+\b)" ~/.ssh/config | awk '{print $2}' | grep $filter | fzf -e)
+    if [ $target ]; then
+        echo "Remoting into: $target"
+        ssh $target
+    fi
 }
