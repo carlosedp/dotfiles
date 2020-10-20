0e29fc6ce84546c9d53da0c5b5ce6e3ff67175d2
Carlos de Paula <me@carlosedp.com>
2020-10-20 12:34:16 -0300

    Adjust fzf

--- rc/fzf.zsh
+++ rc/fzf.zsh
@@ -1,19 +1,19 @@
 # Setup fzf
 # ---------
-if [ ! -x "$(command -v fzf)" ] > /dev/null 2>&1; then
+if [ ! -x $(command -v fzf) ] > /dev/null 2>&1; then
     echo "Error: fzf binary not installed"
     return
 fi
 
-if [[ ! -d "$HOME/.fzf" ]]; then
+if [[ ! -d $HOME/.fzf ]]; then
     echo "Error: .fzf directory not installed"
     return
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "$HOME/.fzf/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source $HOME/.fzf/shell/completion.zsh 2> /dev/null
 
 # Key bindings
 # ------------
-source "$HOME/.fzf/shell/key-bindings.zsh"
+source $HOME/.fzf/shell/key-bindings.zsh
