aa6c61caa9a8bd2cc5a4fd7ce9bf834eb0ccc6f9
Carlos de Paula <me@carlosedp.com>
2020-10-22 17:35:59 -0300

    History

--- shellconfig/exports.sh
+++ shellconfig/exports.sh
@@ -1,5 +1,6 @@
 # Larger bash history (allow 32³ entries; default is 500)
 export HISTSIZE=50000000;
+export SAVEHIST=50000000;
 export HISTFILESIZE=$HISTSIZE;
 export HISTCONTROL=ignoredups;
 # Make some commands not show up in history
